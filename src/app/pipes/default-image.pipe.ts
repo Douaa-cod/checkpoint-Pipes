bc6e39f340e37db0b34bb7f35d312075e1f50b59
Douaa yazidi <douaayazidi@gmail.com>
2020-08-16 00:07:37 +0100

    rectif

--- src/app/pipes/default-image.pipe.ts
+++ src/app/pipes/default-image.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DefaultImagePipe implements PipeTransform {
 
   transform(value: string): string {
-    if (!value){
+    if (!value || value == " "){
       return "tim_logo.png"
     }
     return value
